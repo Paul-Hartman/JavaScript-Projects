057a20075e445eff614380cc3af5f4a5d5ee8ab0
Paul Hartman <78393505+Paul-Hartman@users.noreply.github.com>
2021-10-13 01:37:18 +0200

    Create JavaScript_3.js

---
+++ Basic JavaScript Projects/JavaScript/JavaScript_3.js
@@ -0,0 +1,4 @@
+function displayType(universe) {
+    var universeType = universe.getAttribute('data-universe-type');
+    alert(universeType + '\'s strongest Wizard is ' + universe.innerHTML + '.')
+}
